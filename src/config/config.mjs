1d11b49f121496402a01e13a3e5bc2d2aa6a0b16
ShadyJoseph <shadyjosephabadeer2003@gmail.com>
2024-09-13 17:03:08 +0300

    configs

--- src/config/config.mjs
+++ src/config/config.mjs
@@ -9,7 +9,7 @@ export const APP_CONFIG = {
     cookieSecret: process.env.COOKIE_SECRET || 'defaultCookieSecret',
     sessionMaxAge: parseInt(process.env.SESSION_MAX_AGE, 10) || 1000 * 60 * 60 * 2, // Default: 2 hours
     jwtSecret: process.env.JWT_SECRET || 'defaultJwtSecret',
-    mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/express-node', 
+    mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/express-node', 
     discordClientId: process.env.DISCORD_CLIENT_ID || '',
     discordClientSecret: process.env.DISCORD_CLIENT_SECRET || '',
 };
